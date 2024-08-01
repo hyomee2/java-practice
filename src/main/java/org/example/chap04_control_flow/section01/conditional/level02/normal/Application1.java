b173226fb7e55ca54d8902fed28e49cf894ddc58
hyomee2 <s5763305@naver.com>
2024-08-01 15:11:07 +0900

    [fix]20240801 짝홀수 판별 2 println -> print

--- src/main/java/org/example/chap04_control_flow/section01/conditional/level02/normal/Application1.java
+++ src/main/java/org/example/chap04_control_flow/section01/conditional/level02/normal/Application1.java
@@ -8,7 +8,7 @@ public class Application1 {
          * 홀수이면 "홀수다.", 홀수가 아니면 "짝수다." 라고 출력하세요.
          * 단, 1~10 사이의 정수가 아닌 경우 "반드시 1~10 사이의 정수를 입력해야 합니다." 를 출력하세요.
          * */
-        System.out.println("정수를 하나 입력하세요 5: ");
+        System.out.print("정수를 하나 입력하세요 5: ");
         Scanner scanner = new Scanner(System.in);
 
         int num = scanner.nextInt();
