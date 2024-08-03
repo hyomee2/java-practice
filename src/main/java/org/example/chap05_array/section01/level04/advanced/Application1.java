55b319b51e8202d0f53e43b14b230110b68201a7
hyomee2 <s5763305@naver.com>
2024-08-03 21:01:37 +0900

    [fix] 20240802 로또 번호 생성기
    - 랜덤없이 생성되는 로직 추가

--- src/main/java/org/example/chap05_array/section01/level04/advanced/Application1.java
+++ src/main/java/org/example/chap05_array/section01/level04/advanced/Application1.java
@@ -12,8 +12,15 @@ public class Application1 {
          * */
         int[] lotto = new int[6];
 
-        for (int i = 0; i < lotto.length; i++)
+        for (int i = 0; i < lotto.length; i++) {
             lotto[i] = (int)(Math.random() * 45) + 1;
+            for (int j = 0; j < i; j++) {
+                if (lotto[i] == lotto[j]) {
+                    i--;
+                    break;
+                }
+            }
+        }
 
         Arrays.sort(lotto);
 
