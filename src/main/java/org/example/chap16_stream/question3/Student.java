8658ec04a633c7469ffe21fa2a6c5cd954e817f9
hyomee2 <s5763305@naver.com>
2024-08-16 15:22:33 +0900

    [feat] 240813 Question3 학생 성적 처리

---
+++ src/main/java/org/example/chap16_stream/question3/Student.java
@@ -0,0 +1,26 @@
+package org.example.chap16_stream.question3;
+
+public class Student {
+
+    private String name;
+    private String subject;
+    private int score;
+
+    public Student(String name, String subject, int score) {
+        this.name = name;
+        this.subject = subject;
+        this.score = score;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public int getScore() {
+        return score;
+    }
+}
